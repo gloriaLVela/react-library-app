ffbe27f5618d8ae216773a4c92f2afe943d5a10b
Gloria Vela <gloria.laura.vela@gmail.com>
2019-02-18 06:46:10 -0600

    Conditional rendering

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ let bookList = [
 	{"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
 ]
 
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
 	return (
 		<section>
 			<h2>{title}</h2>
 			<p>by: {author}</p>
 			<p>Pages: {pages} pages</p>
+			<p>Free Bookmark Today: {freeBookmark ? 'yes!': 'no!'}</p>
 		</section>
 	)
 }
@@ -23,30 +24,51 @@ const Book = ({title, author, pages}) => {
 // Wrap multiple components inside one DIV
 // 
 // Map the data in books into the Book component
+const Hiring = () => 
+	<div>
+		<p>The library is hiring. Go to www.library.com/jobs for more.</p>
+	</div>
+
+const NotHiring = () => 
+	<div>
+		<p>The library is not hiring. Check back later for more info.</p>
+	</div>
+
 class Library extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			open: true
-		}
+	
+	state = { 
+		open: true,
+		freeBookmark: false,
+		hiring: true
+	}
+
+	toggleOpenClosed = () => {
+		this.setState(prevState => ({
+			open: !prevState.open
+		}))
 	}
 	render() {
 		const { books } = this.props
 		return (
 			<div>
+			
+				{this.state.hiring ? <Hiring /> : <NotHiring />}
 				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+				<button onClick={this.toggleOpenClosed}>Change</button>
 				{books.map(
 					(book, i) => 
 						<Book 
 							key={i}
 							title={book.title} 
 							author={book.author} 
-							pages={book.pages}/>
+							pages={book.pages}
+							freeBookmark={this.state.freeBookmark}/>
 				)}
 			</div>
 		)
 	}
 }
+
 
 // Library is the main component
 // Pass the booklist to the component using the parameter books
